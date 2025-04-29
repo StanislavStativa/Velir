86db2126a3323b5ff75f2fb756e093d6cd0897c7
Stanislav Stativa <stas.stativa@gmail.com>
2025-04-29 19:13:17 +0300

    add sitecore edge url

--- apps/demo/src/Scripts.tsx
+++ apps/demo/src/Scripts.tsx
@@ -3,6 +3,7 @@ import { EditingScripts } from '@sitecore-jss/sitecore-jss-nextjs';
 import BYOC from 'src/byoc';
 import CdpPageView from '@/components/sxa/CdpPageView';
 import FEAASScripts from '@/components/sxa/FEAASScripts';
+import config from 'temp/config';
 
 const Scripts = (): JSX.Element => {
   return (
@@ -10,7 +11,7 @@ const Scripts = (): JSX.Element => {
       <BYOC />
       <CdpPageView />
       <FEAASScripts />
-      <EditingScripts />
+      <EditingScripts sitecoreEdgeUrl={config.sitecoreEdgeUrl} />
     </>
   );
 };
